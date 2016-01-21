8e7d2a653f2fff447f96d63ee336e37eaae55bef
Jan Sende <jan.sende@uni-jena.de>
2016-01-21 19:12:36 +0100

    Fix

--- series/management/commands/create_anonymous.py
+++ series/management/commands/create_anonymous.py
@@ -9,13 +9,20 @@ class Command(BaseCommand):
     help = 'Creates the user "Anonymous", used for populating the database with liblist data.'
     def handle(self, *args, **options):
         self.stdout.write('Creating Anonymous User...')
-        if User.objects.filter(username='anonymous6115').count() > 0:
+        if User.objects.filter(username='anonymous').count() > 0:
             raise CommandError('The anonymous user already exists!')
+        user             = User()
+        user.username    = 'anonymous'
+        user.first_name  = 'Anon'
+        user.last_name   = ''
+        user.email       = ''
+        user.save()
+
         profile               = UserProfile()
+        profile.user          = user
         profile.title         = ''
         profile.degree        = 'Master of Disaster'
         profile.about_me      = 'Trolling since the beginning...'
-        #THIS TOO IS BAD
         profile.image         = File(open('./series/management/commands/anonymous.png','rb'))
         profile.website       = ''
         profile.phone         = ''
@@ -25,11 +32,4 @@ class Command(BaseCommand):
         profile.show_realname = True
         profile.save()
 
-        user             = User()
-        user.username    = 'anonymous6115'
-        user.first_name  = 'Anon'
-        user.last_name   = ''
-        user.email       = ''
-        user.userprofile = profile
-        user.save()
         self.stdout.write('Done')
